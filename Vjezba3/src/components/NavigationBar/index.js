3e69be68ba7113e3fd45fcde5c303327b8c9bbc6
TomislavP98 <tomislav.petrov98@gmail.com>
2021-02-17 19:32:07 +0100

    Vjezba2

---
+++ Vjezba3/src/components/NavigationBar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { navs as navTabs } from '../../constants/const'
+import styles from './style.module.css'
+
+const NavigationBar = ({ activeTab, useThisStyle }) => (
+  <nav className={styles[useThisStyle || 'navigationBar']}>
+      {navTabs.map(tab => <li className={tab=== activeTab ? styles.active : ''}>
+          {tab}</li>
+      )}
+  </nav>
+)
+
+export default NavigationBar
