aa67b77bbbf5662360d28cc63d44db1af9bfb3a0
TomislavP98 <tomislav.petrov98@gmail.com>
2021-02-17 19:33:07 +0100

    Vjezba2

---
+++ Vjezba4/src/layouts/headerFooter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavigationHeader from '../modules/NavigationHeader'
+import Footer from '../modules/Footer'
+
+const HeaderFooterLayout = ({children}) => (
+  <>
+    <NavigationHeader />
+      {children}
+    <Footer />
+  </>
+)
+
+export default HeaderFooterLayout
